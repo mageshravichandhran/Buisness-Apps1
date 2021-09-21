8f16eb02172c2d6b6ec8d1a82cec73bb5bb8fd59
mageshravichandhran <58700838+mageshravichandhran@users.noreply.github.com>
2021-09-21 18:40:40 +0530

    Add files via upload

---
+++ Mp3Player/Class1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace Mp3Player
+{
+  public class Song
+    {
+       public String Title;
+       public String Artist;
+      public  int minutes;
+       public int seconds;
+
+
+        public Song(String Title, String Artist,int minutes,int seconds)
+        {
+
+            this.Title = Title;
+            this.Artist = Artist;
+            this.minutes = minutes;
+            this.seconds = seconds;
+
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine("Title of the Song : {0}",this.Title);
+            Console.WriteLine("Artist of the Song : {0}", this.Artist);
+            Console.WriteLine("Number of Minutes : {0}", this.minutes);
+            Console.WriteLine("Number of Seconds : {0}", this.seconds);
+        }
+
+
+
+
+    }
+
+
+    public class Player
+    {
+        Dictionary<String, Song> songs;
+        public Player(Dictionary<string, Song> songs) {
+
+            this.songs = songs;
+
+
+          }
+
+
+        public void Add()
+        {
+            bool start = true;
+            while (start)
+            {
+                Console.WriteLine("Enter the Name of the Song");
+                String title1 = Console.ReadLine();
+
+                Console.WriteLine("Enter the Name of the Artist");
+                string name1 = Console.ReadLine();
+
+                Console.WriteLine("Enter the minutes of the song");
+                int minutes = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the seconds of the song");
+                int seconds = Convert.ToInt32(Console.ReadLine());
+
+
+               
+
+                Song newsong = new Song(title1, name1, minutes, seconds);
+                this.songs.Add(newsong.Title, newsong);
+
+                Console.WriteLine("AddedSuccessfully");
+
+                Console.WriteLine("Add more? Y or N");
+
+                string decision = Console.ReadLine();
+
+                start = decision == "Y" ? true : false;
+               
+
+
+            }
+           
+
+
+
+        }
+
+        public void Getsong(string title)
+        {
+           foreach(KeyValuePair <string,Song> keyValuePair in this.songs)
+            {
+                if (keyValuePair.Key == title)
+                {
+                    keyValuePair.Value.Display();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("not found");
+                }
+               
+            }
+        }
+
+
+        public void Removesong(string title)
+        {
+            foreach (KeyValuePair<string, Song> keyValuePair in this.songs)
+            {
+                if (keyValuePair.Key == title)
+                {
+                    this.songs.Remove(title);
+                }
+                else
+                {
+                    Console.WriteLine("not found");
+                }
+
+            }
+        }
+
+
+        public void printAllsongs()
+        {
+            foreach (KeyValuePair<string, Song> keyValuePair in this.songs)
+            {
+                keyValuePair.Value.Display();
+
+            }
+
+        }
+        
+
+        public void printSongbyArtist(string artist)
+        {
+            foreach (KeyValuePair<string, Song> keyValuePair in this.songs)
+            {
+                if (keyValuePair.Key == artist)
+                {
+                    keyValuePair.Value.Display();
+                }
+                else
+                {
+                    Console.WriteLine("not found");
+                }
+
+            }
+        }
+
+
+        public void GetSize()
+        {
+            Console.WriteLine( this.songs.Count);
+        }
+
+
+
+
+    }
+
+
+      
+}
---
+++ Mp3Player/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Mp3Player;
+namespace Mp3Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the Name of the Song");
+            //String title1 = Console.ReadLine();
+
+            //Console.WriteLine("Enter the Name of the Artist");
+            //string Name1 = Console.ReadLine();
+
+            //Console.WriteLine("Enter the minutes of the song");
+            //int minutes = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the seconds of the song");
+            //int Seconds =Convert.ToInt32( Console.ReadLine());
+
+            Song Song1 = new Song("h","e",1,2);
+
+
+            Dictionary<string, Song> Songs = new Dictionary<string, Song>();
+
+            Songs.Add(Song1.Title, Song1);
+
+
+            Player firsttry = new Player(Songs);
+            
+          
+            bool quit = false;
+            while (quit==false)
+            {
+
+                Console.WriteLine("Enter A to Add song");
+
+                Console.WriteLine("Enter G to get song");
+
+                Console.WriteLine("Enter R to Remove a song");
+
+                Console.WriteLine("Enter P TO PRINT ALL Songs");
+
+
+                Console.WriteLine("enter B to Print songs by Artists");
+
+                Console.WriteLine("Enter S to get the size of the playlist");
+
+                Console.WriteLine("Enter Q to quit");
+
+
+                string option = Console.ReadLine();
+
+                if (option == "A")
+                {
+                    firsttry.Add();
+                }
+                if (option == "G")
+                {
+                    Console.WriteLine("enter song to get");
+                    string songtitle = Console.ReadLine();
+                    firsttry.Getsong(songtitle);
+                }
+
+                if (option =="R")
+                {
+                    Console.WriteLine("Enter song to remove");
+                    string songtitle = Console.ReadLine();
+                    firsttry.Removesong(songtitle);
+                }
+
+                if (option == "P")
+                {
+                    firsttry.printAllsongs();
+                }
+
+                if (option == "B")
+                {
+                    Console.WriteLine("Enter artist name to print");
+                    string Artist = Console.ReadLine();
+                    firsttry.printSongbyArtist(Artist);
+                }
+
+                if (option == "S")
+                {
+                    firsttry.GetSize();
+                }
+
+                if (option == "Q")
+                {
+                    quit = true;
+                    break;
+                }
+
+            }
+          
+
+            
+
+           
+        }
+    }
+}
